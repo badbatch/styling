faa70799e49e3d8cfe8226a31767fb78ceffe55f
Dylan Aubrey <dylanaubrey@gmail.com>
2019-12-31 23:40:56 +0000

    fix: adding even more logging

--- packages/babel-plugin-transform-styling/src/helpers/set-identifier-in-exports-args.ts
+++ packages/babel-plugin-transform-styling/src/helpers/set-identifier-in-exports-args.ts
@@ -14,10 +14,14 @@ export default function setIdentifierInExportsArgs(exportDeclarations: Array<Nod
       {
         CallExpression(path, state) {
           if (path.findParent(node => node.isTaggedTemplateExpression())) {
-            info(`Entering CallExpression`);
+            info(`Entering call expression`);
+            info(`Find parent`);
             const variable = path.findParent(node => node.isVariableDeclarator()) as NodePath<VariableDeclarator>;
+            info(`Get id`);
             const name = (variable.get("id") as NodePath<Identifier>).node.name;
+            info(`Get arguments`);
             const args = path.get("arguments");
+            info(`Replace call expression`);
             path.replaceWith(callExpression(path.node.callee, [...args.map(arg => arg.node), stringLiteral(name)]));
             path.skip();
           }
