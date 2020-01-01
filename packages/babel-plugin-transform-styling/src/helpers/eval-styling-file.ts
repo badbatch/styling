14499dc51d9461401f697abe1ce3f74d316d9d31
Dylan Aubrey <dylanaubrey@gmail.com>
2020-01-01 19:55:23 +0000

    fix: adding logging to eval styling file script

--- packages/babel-plugin-transform-styling/src/helpers/eval-styling-file.ts
+++ packages/babel-plugin-transform-styling/src/helpers/eval-styling-file.ts
@@ -1,8 +1,11 @@
 import { BabelFileResult, transform } from "@babel/core";
 import nodeEval from "node-eval";
 import { StylingNamedExports } from "../types";
+import { info } from "./log";
 
 export default function evalStylingFile(code: string, filename: string): StylingNamedExports {
+  info(`Transforming code to commonjs`);
+
   const transformed = transform(code, {
     filename,
     plugins: ["@babel/plugin-transform-modules-commonjs"],
@@ -15,7 +18,9 @@ export default function evalStylingFile(code: string, filename: string): Styling
         },
       ],
     ],
+    sourceType: "module",
   }) as BabelFileResult;
 
+  info(`Evaluating transformed code`);
   return nodeEval(transformed.code as string, filename);
 }
