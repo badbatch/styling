22d729d5b0f12cbaff3f24d03c81dfad3de7dcdf
Dylan Aubrey <dylanaubrey@gmail.com>
2020-01-02 12:42:51 +0000

    fix: make file writing sync

--- packages/styled/src/helpers/write-css.ts
+++ packages/styled/src/helpers/write-css.ts
@@ -1,7 +1,7 @@
-import { appendFile } from "fs-extra";
+import { appendFileSync } from "fs-extra";
 import { resolve } from "path";
 import { STYLING_CSS_FILENAME } from "../constants";
 
 export default function writeCSS(css: string, outputPath: string) {
-  appendFile(resolve(outputPath, STYLING_CSS_FILENAME), css, { encoding: "utf-8" });
+  appendFileSync(resolve(outputPath, STYLING_CSS_FILENAME), css, { encoding: "utf-8" });
 }
