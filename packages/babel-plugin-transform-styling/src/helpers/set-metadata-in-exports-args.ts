313480729d7e60944eab5772c24bb4a0dc009801
Dylan Aubrey <dylanaubrey@gmail.com>
2020-01-03 10:43:48 +0000

    feat: change way call expression is replaced

--- packages/babel-plugin-transform-styling/src/helpers/set-metadata-in-exports-args.ts
+++ packages/babel-plugin-transform-styling/src/helpers/set-metadata-in-exports-args.ts
@@ -1,6 +1,13 @@
 import { NodePath } from "@babel/core";
-import traverse from "@babel/traverse";
-import { ExportNamedDeclaration, Identifier, VariableDeclarator, callExpression, stringLiteral } from "@babel/types";
+import {
+  CallExpression,
+  ExportNamedDeclaration,
+  Identifier,
+  TaggedTemplateExpression,
+  VariableDeclaration,
+  callExpression,
+  stringLiteral,
+} from "@babel/types";
 import { info } from "@styling/helpers";
 
 export default function setMetadataInExportsArgs(
@@ -12,32 +19,24 @@ export default function setMetadataInExportsArgs(
   return exportDeclarations.forEach(declaration => {
     info(`Entering export declaration`);
 
-    traverse(
-      declaration.node,
-      {
-        CallExpression(path) {
-          if (path.findParent(node => node.isTaggedTemplateExpression())) {
-            info(`Entering call expression`);
-            info(`Find parent`);
-            const variable = path.findParent(node => node.isVariableDeclarator()) as NodePath<VariableDeclarator>;
-            info(`Get id`);
-            const name = (variable.get("id") as NodePath<Identifier>).node.name;
-            info(`Get arguments`);
-            const args = path.get("arguments");
-            info(`Replace call expression`);
-            path.replaceWith(
-              callExpression(path.node.callee, [
-                ...args.map(arg => arg.node),
-                stringLiteral(name),
-                stringLiteral(sourceDir),
-              ]),
-            );
-            path.skip();
-          }
-        },
-      },
-      declaration.scope,
-      declaration,
+    const variableDeclaratorPath = (declaration.get("declaration") as NodePath<VariableDeclaration>).get(
+      "declarations",
+    )[0];
+
+    const name = (variableDeclaratorPath.get("id") as NodePath<Identifier>).node.name;
+
+    const callExpressionPath = (variableDeclaratorPath.get("init") as NodePath<TaggedTemplateExpression>).get(
+      "tag",
+    ) as NodePath<CallExpression>;
+
+    const argumentsPath = callExpressionPath.get("arguments");
+
+    callExpressionPath.replaceWith(
+      callExpression(callExpressionPath.node.callee, [
+        ...argumentsPath.map(argumentPath => argumentPath.node),
+        stringLiteral(name),
+        stringLiteral(sourceDir),
+      ]),
     );
   });
 }
