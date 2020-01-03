c0cc59e5f880f651321e0eb15e1a5779cde4770e
Dylan Aubrey <dylanaubrey@gmail.com>
2020-01-03 11:27:25 +0000

    fix: change way exports args are collated

--- packages/babel-plugin-transform-styling/src/helpers/get-exports-args.ts
+++ packages/babel-plugin-transform-styling/src/helpers/get-exports-args.ts
@@ -1,39 +1,40 @@
 import { NodePath } from "@babel/core";
-import traverse from "@babel/traverse";
-import { ExportNamedDeclaration, Identifier, VariableDeclarator } from "@babel/types";
+import {
+  CallExpression,
+  ExportNamedDeclaration,
+  Identifier,
+  TaggedTemplateExpression,
+  VariableDeclaration,
+} from "@babel/types";
 import { IDENTIFIER, STRING_LITERAL } from "../constants";
 import { ExportsArgsResult } from "../types";
 
 export default function getExportsArgs(exportDeclarations: Array<NodePath<ExportNamedDeclaration>>) {
   return exportDeclarations.reduce(
     (args: ExportsArgsResult, declaration) => {
-      traverse(
-        declaration.node,
-        {
-          CallExpression(path) {
-            if (path.findParent(node => node.isTaggedTemplateExpression())) {
-              const variable = path.findParent(node => node.isVariableDeclarator()) as NodePath<VariableDeclarator>;
-              const name = (variable.get("id") as NodePath<Identifier>).node.name;
-              const firstArg = path.get("arguments")[0];
-              const obj = { type: "", value: "" };
+      const variableDeclaratorPath = (declaration.get("declaration") as NodePath<VariableDeclaration>).get(
+        "declarations",
+      )[0];
 
-              if (firstArg.isIdentifier()) {
-                obj.type = IDENTIFIER;
-                obj.value = firstArg.node.name;
-                args.identifiers.push(obj.value);
-              } else if (firstArg.isStringLiteral()) {
-                obj.type = STRING_LITERAL;
-                obj.value = firstArg.node.value;
-              }
+      const name = (variableDeclaratorPath.get("id") as NodePath<Identifier>).node.name;
 
-              args.map.set(name, obj);
-            }
-          },
-        },
-        declaration.scope,
-        declaration,
-      );
+      const callExpressionPath = (variableDeclaratorPath.get("init") as NodePath<TaggedTemplateExpression>).get(
+        "tag",
+      ) as NodePath<CallExpression>;
 
+      const firstArgumentsPath = callExpressionPath.get("arguments")[0];
+      const obj = { type: "", value: "" };
+
+      if (firstArgumentsPath.isIdentifier()) {
+        obj.type = IDENTIFIER;
+        obj.value = firstArgumentsPath.node.name;
+        args.identifiers.push(obj.value);
+      } else if (firstArgumentsPath.isStringLiteral()) {
+        obj.type = STRING_LITERAL;
+        obj.value = firstArgumentsPath.node.value;
+      }
+
+      args.map.set(name, obj);
       return args;
     },
     { identifiers: [], map: new Map() },
