f328149344092158d9246ede30e0393cb7b84850
Dylan Aubrey <dylanaubrey@gmail.com>
2020-01-06 16:02:07 +0000

    test: add additional scenario for styling function

--- packages/core/src/main.test.tsx
+++ packages/core/src/main.test.tsx
@@ -3,16 +3,78 @@ import React from "react";
 import styling from "./main";
 
 describe("styling", () => {
-  it("SHOULD return the correct component with the correct class names", () => {
-    const Container = styling("div", ["checked", "disabled"], ["checked", "disabled"], {
-      base: "container",
-      checked: "container--checked",
-      "checked--disabled": "container--checked--disabled",
-      disabled: "container--disabled",
-    });
+  const PrimaryButton = styling(
+    "button",
+    [
+      "block",
+      "disabled",
+      "error",
+      "externalText",
+      "inverse",
+      "noText",
+      ["size", ["xs", "sm", "md"]],
+      "stretch",
+      ["variant", ["primary", "secondary", "link"]],
+    ],
+    [
+      "block",
+      "disabled",
+      "error",
+      "externalText",
+      "inverse",
+      "noText",
+      "stretch",
+      "variant::link",
+      "size::xs",
+      "size::sm",
+    ],
+    {
+      base: "button__primary-button-1842608486",
+      block: "button__primary-button-1842608486--block",
+      disabled: "button__primary-button-1842608486--disabled",
+      error: "button__primary-button-1842608486--error",
+      "external-text": "button__primary-button-1842608486--external-text",
+      inverse: "button__primary-button-1842608486--inverse",
+      "no-text": "button__primary-button-1842608486--no-text",
+      "no-text--size-sm": "button__primary-button-1842608486--no-text--size-sm",
+      "no-text--size-xs": "button__primary-button-1842608486--no-text--size-xs",
+      stretch: "button__primary-button-1842608486--stretch",
+      "variant-link": "button__primary-button-1842608486--variant-link",
+    },
+  );
 
-    expect(mount(<Container checked disabled />).html()).toBe(
-      `<div disabled="\" class="container container--checked--disabled container--checked container--disabled"></div>`,
+  describe("WHEN a tag name is passed in as the component", () => {
+    it("SHOULD return the correct component with the correct class names", () => {
+      expect(mount(<PrimaryButton noText size="sm" />).html()).toBe(
+        `<button class=\"button__primary-button-1842608486 button__primary-button-1842608486--no-text--size-sm button__primary-button-1842608486--no-text\"></button>`,
+      );
+    });
+  });
+
+  describe("WHEN a React component is passed in as the component", () => {
+    const PrimaryButtonStyledSpan = styling(
+      PrimaryButton,
+      [
+        "block",
+        "disabled",
+        "error",
+        "externalText",
+        "inverse",
+        "noText",
+        ["size", ["xs", "sm", "md"]],
+        "stretch",
+        ["variant", ["primary", "secondary", "link"]],
+      ],
+      [],
+      {
+        base: "button__primary-button-styled-span-759899086",
+      },
     );
+
+    it("SHOULD return the correct component with the correct class names", () => {
+      expect(mount(<PrimaryButtonStyledSpan noText size="sm" />).html()).toBe(
+        `<button class=\"button__primary-button-1842608486 button__primary-button-1842608486--no-text--size-sm button__primary-button-1842608486--no-text button__primary-button-styled-span-759899086\"></button>`,
+      );
+    });
   });
 });
