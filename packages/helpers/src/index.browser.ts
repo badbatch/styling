e7c8d9ac69043e1742989d055500dc1991717cab
Dylan Aubrey <dylanaubrey@gmail.com>
2020-01-08 00:52:12 +0000

    fix: make logger avaialbe in client bundle

--- packages/helpers/src/index.browser.ts
+++ packages/helpers/src/index.browser.ts
@@ -1,2 +1,3 @@
 export { default as buildMapKeyFromPropKeyCombo } from "./build-map-key-from-prop-key-combo";
 export { default as generatePropKeyCombos } from "./generate-prop-key-combos";
+export * from "./log";
