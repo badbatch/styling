604e85965ca81d8531b41b0a3efce8e5fe573cd8
Dylan Aubrey <dylanaubrey@gmail.com>
2020-01-27 23:32:37 +0000

    fix: sort media queries overriding each other

--- packages/styled/src/constants.ts
+++ packages/styled/src/constants.ts
@@ -1,2 +1,3 @@
 export const CLASSNAME = "className" as const;
 export const CSS_FILE_EXT = ".css" as const;
+export const MEDIA_CSS_RULE = "@media" as const;
--- packages/styled/src/helpers/merge-new-css-into-existing.ts
+++ packages/styled/src/helpers/merge-new-css-into-existing.ts
@@ -1,15 +1,22 @@
+import { assignWith } from "lodash";
 import postcss from "postcss";
-import postcssJs from "postcss-js";
+import postcssJs, { CSSObject } from "postcss-js";
+import { MEDIA_CSS_RULE } from "../constants";
+
+function mergeCSS(existingCSS: CSSObject, newCSS: CSSObject): CSSObject {
+  return assignWith({}, existingCSS, newCSS, (objValue, srcValue, key) => {
+    if (!key?.startsWith(MEDIA_CSS_RULE)) return undefined;
+
+    return mergeCSS(objValue, srcValue);
+  });
+}
 
 export default function mergeNewCSSIntoExisting(newCSS: string, existingCSS: string) {
   const newCSSOjb = postcssJs.objectify(postcss.parse(newCSS));
   const existingCSSObj = postcssJs.objectify(postcss.parse(existingCSS));
 
-  return postcss().process(
-    { ...existingCSSObj, ...newCSSOjb },
-    {
-      from: undefined,
-      parser: postcssJs.parse,
-    },
-  ).css;
+  return postcss().process(mergeCSS(existingCSSObj, newCSSOjb), {
+    from: undefined,
+    parser: postcssJs.parse,
+  }).css;
 }
